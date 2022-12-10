4875220252403a1206f2b45dbdb5ea751c90d095
satriosws <ststio1018@gmail.com>
2022-12-10 21:55:04 +0700

    update application/views/index/index.php

--- application/views/index/index.php
+++ application/views/index/index.php
@@ -2,7 +2,7 @@
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2"><?php echo !empty($title) ? $title : null ?></h1>
     </div>
-    <div class="alert alert-primary">
+    <div class="alert alert-primary" align="center">
         <h5>Selamat Datang Satrio Wicaksono </h5>
         <span>Semoga harimu menyenangkan</span>
     </div>
