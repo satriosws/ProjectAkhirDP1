34e839a6acc73cbfc99d908f7505f1f41c9304c2
satriosws <ststio1018@gmail.com>
2022-12-11 08:00:54 +0700

    update application/views/index/index.php perubahan tampilan pada manu dashboard

--- application/views/index/index.php
+++ application/views/index/index.php
@@ -1,9 +1,9 @@
-<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+<main class="ms-sm-auto px-md-4 bg-light">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2"><?php echo !empty($title) ? $title : null ?></h1>
     </div>
-    <div class="alert alert-primary" align="center">
-        <h5>Selamat Datang Satrio Wicaksono </h5>
-        <span>Semoga harimu menyenangkan</span>
+    <div class="mt-4 p-5 bg-info text-dark rounded" align="center">
+    <h1>Selamat Datang Satrio Wicaksono</h1>
+        <p>Semoga harimu menyenangkan</p> 
     </div>
 </main>
