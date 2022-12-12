f81651afa450e7038ce12493362dac94f9cbd56e
satriosws <ststio1018@gmail.com>
2022-12-12 16:25:54 +0700

    update application/views/index/grafik_empat.php

--- application/views/index/grafik_empat.php
+++ application/views/index/grafik_empat.php
@@ -1,6 +1,7 @@
-<main class="ms-sm-auto px-md-4 bg-primary">
+<main class="ms-sm-auto px-md-4 bg-secondary">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2"><?php echo !empty($title) ? $title : null ?></h1>
+        <h3>Jumlah Masuk : 1000 peserta</h3>
     </div>
     <div class="row">
         <div class="col-md-12">
@@ -23,7 +24,7 @@
                 text: title
             },
             tooltip: {
-                pointFormat: '{series}: <b>{point.jumlah:.1f} Pendaftar Prestasi</b>'
+                pointFormat: '{series}: <b>{point.jumlah:.1f} Pendaftar</b>'
             },
             accessibility: {
                 point: {
